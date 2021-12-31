b9787ba1afd6432127733614fa091d150e589f54
Emmmanuel Garcia <Emmanuel_801@hotmail.com>
2021-12-31 00:04:49 -0700

    updated service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -16,35 +16,67 @@ const FILES_TO_CACHE = [
   "/icons/icon-384x384.png",
   "/icons/icon-512x512.png"
 ];
-// installed service worker
-self.addEventListener('install', function (e) {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      console.log('installing cache : ' + CACHE_NAME)
-      return cache.addAll(FILES_TO_CACHE)
-    })
-  )
-});
-//  active the service worker 
-self.addEventListener('activate', function (e) {
-  e.waitUntil(
-    caches.keys().then(function (keyList) {
-      let cacheKeeplist = keyList.filter(function (key) {
-        return key.indexOf(APP_PREFIX);
-      });
-      cacheKeeplist.push(CACHE_NAME);
+// // installed service worker
+// self.addEventListener('install', function (e) {
+//   e.waitUntil(
+//     caches.open(CACHE_NAME).then(function (cache) {
+//       console.log('installing cache : ' + CACHE_NAME)
+//       return cache.addAll(FILES_TO_CACHE)
+//     })
+//   )
+// });
+// //  active the service worker 
+// self.addEventListener('activate', function (e) {
+//   e.waitUntil(
+//     caches.keys().then(function (keyList) {
+//       let cacheKeeplist = keyList.filter(function (key) {
+//         return key.indexOf(APP_PREFIX);
+//       });
+//       cacheKeeplist.push(CACHE_NAME);
 
+//       return Promise.all(
+//         keyList.map(function (key, i) {
+//           if (cacheKeeplist.indexOf(key) === -1) {
+//             console.log('deleting cache : ' + keyList[i]);
+//             return caches.delete(keyList[i]);
+//           }
+//         })
+//       );
+//     })
+//   );
+// });
+
+
+// Install the service worker
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Your files were pre-cached successfully!');
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+
+  self.skipWaiting();
+});
+
+// Activate the service worker and remove old data from the cache
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(keyList => {
       return Promise.all(
-        keyList.map(function (key, i) {
-          if (cacheKeeplist.indexOf(key) === -1) {
-            console.log('deleting cache : ' + keyList[i]);
-            return caches.delete(keyList[i]);
+        keyList.map(key => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log('Removing old cache data', key);
+            return caches.delete(key);
           }
         })
       );
     })
   );
+
+  self.clients.claim();
 });
+
 
 self.addEventListener('fetch', function (e) {
   console.log('fetch request : ' + e.request.url)
