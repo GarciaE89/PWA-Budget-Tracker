7230064fe20b0c686e49dca2e2a8939d2060eedf
Emmmanuel Garcia <Emmanuel_801@hotmail.com>
2022-01-07 12:21:34 -0700

    updated idb.js

--- public/js/idb.js
+++ public/js/idb.js
@@ -23,65 +23,51 @@ request.onsuccess = function(event) {
     }
   };
 
-  // This function will be executed if we attempt to submit a new pizza and there's no internet connection
-function saveRecord(record) {
-    // open a new transaction with the database with read and write permissions 
-    const transaction = db.transaction(['new_budget'], 'readwrite');
+  request.onerror = function (event) {
+    console.log(event.target.errorCode);
+  };
   
-    // access the object store for `new_pizza`
-    const budgetObjectStore = transaction.objectStore('new_budget');
+  function saveRecord(record) {
+    const transaction = db.transaction(["new_budget"], "readwrite");
+    const budgetObjectStore = transaction.objectStore("new_budget");
   
-    // add record to your store with add method
     budgetObjectStore.add(record);
   }
   
-  request.onerror = function(event) {
-    // log error here
-    console.log(event.target.errorCode);
-  };
-
   function uploadBudget() {
-    // open a transaction on your db
-    const transaction = db.transaction(['new_budget'], 'readwrite');
-  
-    // access your object store
-    const budgetObjectStore = transaction.objectStore('new_budget');
-  
-    // get all records from store and set to a variable
+    const transaction = db.transaction(["new_budget"], "readwrite");
+    const budgetObjectStore = transaction.objectStore("new_budget");
     const getAll = budgetObjectStore.getAll();
   
-    // upon a successful .getAll() execution, run this function
-getAll.onsuccess = function() {
-    // if there was data in indexedDb's store, let's send it to the api server
-    if (getAll.result.length > 0) {
-      fetch('/api/transaction', {
-        method: 'POST',
-        body: JSON.stringify(getAll.result),
-        headers: {
-          Accept: 'application/json, text/plain, */*',
-          'Content-Type': 'application/json'
-        }
-      })
-        .then(response => response.json())
-        .then(serverResponse => {
-          if (serverResponse.message) {
-            throw new Error(serverResponse);
-          }
-          // open one more transaction
-          const transaction = db.transaction(['new_budget'], 'readwrite');
-          // access the new_pizza object store
-          const pizzaObjectStore = transaction.objectStore('new_budget');
-          // clear all items in your store
-          pizzaObjectStore.clear();
-
-          alert('All saved budget infot has been submitted!');
+    getAll.onsuccess = function () {
+      if (getAll.result.length > 0) {
+        fetch("/api/transaction/bulk", {
+          method: "POST",
+          body: JSON.stringify(getAll.result),
+          headers: {
+            Accept: "application/json, text/plain, */*",
+            "Content-Type": "application/json",
+          },
         })
-        .catch(err => {
-          console.log(err);
-        });
-    }
-  };
+          .then((response) => response.json())
+          .then((serverResponse) => {
+            if (serverResponse.message) {
+              throw new Error(serverResponse);
+            }
+  
+            const transaction = db.transaction(["new_budget"], "readwrite");
+  
+            const budgetObjectStore = transaction.objectStore("new_budget");
+  
+            budgetObjectStore.clear();
+  
+            alert("All saved budget information has be submitted!");
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
+    };
   }
-
-  // listen for app coming back online
-window.addEventListener('online', uploadBudget);
+  
+  window.addEventListener("online", uploadBudget);
